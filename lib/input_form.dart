7c48567baf4d19415526b61635e1ce8c153e9b40
pocoochan <sayamatsui327@gmail.com>
2024-07-06 23:58:28 +0900

    input page

---
+++ lib/input_form.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class InputForm extends StatefulWidget {
+  const InputForm({super.key});
+
+  @override
+  State<InputForm> createState() => _InputFormState();
+}
+
+class _InputFormState extends State<InputForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _textEditingController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: TextFormField(
+                    controller: _textEditingController,
+                    maxLines: 5,
+                    decoration: const InputDecoration(hintText: '文章を入力してください'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '文章が入力されていません';
+                      }
+                      return null;
+                    })),
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  final fromState = _formKey.currentState!;
+                  if (!fromState.validate()) {
+                    return;
+                  }
+                  debugPrint('text = ${_textEditingController.text}');
+                },
+                child: const Text('変換'))
+          ],
+        ));
+  }
+
+  @override
+  void dispose() {
+    _textEditingController.dispose();
+    super.dispose();
+  }
+}
